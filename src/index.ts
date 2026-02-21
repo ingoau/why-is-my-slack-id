633365c5fe55f7f31e1b0192ba30029b8c698d8d
Ingo Wolf <me@ingowolf.au>
2026-02-21 13:22:43 +1100

    add respodning to messages

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import { SlackApp, SlackEdgeAppEnv, isPostedMessageEvent } from 'slack-cloudflar
 
 export default {
 	async fetch(request: Request, env: SlackEdgeAppEnv, ctx: ExecutionContext): Promise<Response> {
-		const app = new SlackApp({ env }).event('message', async ({ payload }) => {
-			if (isPostedMessageEvent(payload)) {
+		const app = new SlackApp({ env }).event('message', async ({ payload, context }) => {
+			if (isPostedMessageEvent(payload) && payload.channel === process.env.CHANNEL_ID && !payload.thread_ts) {
+				await context.client.chat.postMessage({
+					channel: process.env.CHANNEL_ID,
+					thread_ts: payload.ts,
+					text: 'Test',
+				});
 				console.log(`New message: ${payload.text}`);
 			}
 		});
