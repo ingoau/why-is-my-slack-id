7e45b162afe37cc7c5df1442b9f64fc9c6f77721
Ingo Wolf <me@ingo.au>
2026-08-05 20:52:24 +1000

    add code yes

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { App } from "slack.ts";
+import { env } from "./env";
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: { type: "socket", appToken: process.env.SLACK_APP_TOKEN! },
+});
+
+app.on("message", async (message) => {
+  console.log("a");
+  if (message.user === process.env.SLACK_USER_ID) return;
+  if (message.channel.id !== env.CHANNEL_ID) return;
+
+  await message.reply("test");
+});
+
+await app.start();
