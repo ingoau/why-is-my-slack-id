4967e35e2a828525fa726f2efebd10d5f5e1ffe4
Ingo Wolf <me@ingo.au>
2026-08-10 17:14:26 +1000

    add agentic capabilities

--- ai/index.ts
+++ ai/index.ts
@@ -3,11 +3,11 @@ import { prompt } from "./prompt";
 
 const codex = new Codex();
 
-export function processSlackId() {
+export function processSlackId(slackId: string) {
   const thread = codex.startThread({
     model: "gpt-5.6-sol",
     modelReasoningEffort: "low",
   });
 
-  return thread.runStreamed(prompt + "\nU0923H02Y3B");
+  return thread.run(prompt + "\n" + slackId);
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { App } from "slack.ts";
 import { env } from "./env";
+import { processSlackId } from "./ai";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -10,7 +11,10 @@ app.on("message", async (message) => {
   if (message.thread_ts || (await message.author)?.is_bot) return;
   if (message.channel.id !== env.CHANNEL_ID) return;
 
-  await message.reply("test");
+  if (!message.author?.id) return;
+  const explanation = await processSlackId(message.author?.id);
+
+  await message.reply(explanation.finalResponse);
 });
 
 await app.start();
