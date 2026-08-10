099bbd1897bba2bc515f1830b8044b5144dfa1f2
Ingo Wolf <me@ingo.au>
2026-08-10 17:35:50 +1000

    uh idk what im doing

--- index.ts
+++ index.ts
@@ -1,20 +1,34 @@
-import { App } from "slack.ts";
+import { App } from "@slack/bolt";
 import { env } from "./env";
 import { processSlackId } from "./ai";
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  receiver: { type: "socket", appToken: process.env.SLACK_APP_TOKEN! },
+  socketMode: true,
+  appToken: env.SLACK_APP_TOKEN,
+  token: env.SLACK_BOT_TOKEN,
 });
 
-app.on("message", async (message) => {
-  if (message.thread_ts || (await message.author)?.is_bot) return;
-  if (message.channel.id !== env.CHANNEL_ID) return;
-
-  if (!message.author?.id) return;
-  const explanation = await processSlackId(message.author?.id);
-
-  await message.reply(explanation.finalResponse);
+app.message(async ({ event, say }) => {
+  if ("subtype" in event && event.subtype !== undefined) return;
+  if (event.thread_ts || event.bot_id) return;
+  say({
+    text: "Kevin",
+  });
 });
+
+// const app = new App({
+//   token: process.env.SLACK_BOT_TOKEN,
+//   receiver: { type: "socket", appToken: process.env.SLACK_APP_TOKEN! },
+// });
+
+// app.on("message", async (message) => {
+//   if (message.thread_ts || (await message.author)?.is_bot) return;
+//   if (message.channel.id !== env.CHANNEL_ID) return;
+
+//   if (!message.author?.id) return;
+//   const explanation = await processSlackId(message.author?.id);
+
+//   await message.reply(explanation.finalResponse);
+// });
 
 await app.start();
