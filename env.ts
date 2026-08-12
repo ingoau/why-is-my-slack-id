2eaef5e3e97e3b4a09472cb0ee7c18a3dd75dfbe
Ingo Wolf <me@ingo.au>
2026-08-12 22:29:45 +1000

    add openrouter stuff

--- env.ts
+++ env.ts
@@ -6,6 +6,7 @@ export const env = createEnv({
     SLACK_BOT_TOKEN: z.string().min(1),
     SLACK_APP_TOKEN: z.string().min(1),
     CHANNEL_ID: z.string().min(1),
+    OPENROUTER_KEY: z.string().min(1),
   },
 
   /**
