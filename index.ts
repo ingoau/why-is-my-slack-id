2cdfa4381dad46daec6ead62105daeadf2fce68f
Ingo Wolf <me@ingo.au>
2026-08-12 22:05:58 +1000

    remove commented code

--- index.ts
+++ index.ts
@@ -27,19 +27,4 @@ app.message(async ({ event, say, client }) => {
   });
 });
 
-// const app = new App({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   receiver: { type: "socket", appToken: process.env.SLACK_APP_TOKEN! },
-// });
-
-// app.on("message", async (message) => {
-//   if (message.thread_ts || (await message.author)?.is_bot) return;
-//   if (message.channel.id !== env.CHANNEL_ID) return;
-
-//   if (!message.author?.id) return;
-//   const explanation = await processSlackId(message.author?.id);
-
-//   await message.reply(explanation.finalResponse);
-// });
-
 await app.start();
